c4bd5cc570432e4157894201fe29ef7cb6e3ad02
Telefoan <155188161+Telefoan@users.noreply.github.com>
2025-03-08 15:21:32 -0500

    commented out

--- src/constants/card_data_constants.asm
+++ src/constants/card_data_constants.asm
@@ -70,6 +70,8 @@ DEF CARD_DATA_WEAKNESS                EQU $33 ; 1 byte
 ;DEF CARD_DATA_CATEGORY                EQU $35 ; 2 bytes 
 ;DEF CARD_DATA_POKEDEX_NUMBER          EQU $37 ; 2 bytes
 DEF CARD_DATA_LEVEL                   EQU $39 ; 1 byte 
+;DEF CARD_DATA_LENGTH                  EQU $3a ; 2 bytes (1 for ft, 1 for inches)
+;DEF CARD_DATA_WEIGHT                  EQU $3c ; 2 bytes 
 DEF CARD_DATA_PKMN_DESCRIPTION        EQU $3e ; 2 bytes 
 DEF CARD_DATA_PKMN_FLAGS              EQU $40 ; 1 byte 
 
